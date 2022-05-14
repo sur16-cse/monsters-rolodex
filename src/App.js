dc5091a72a516fd0b51dee322ba060168ffd639f
surbhi kumari <surbhikumaridav@gmail.com>
2022-05-14 18:57:12 +0530

    class based

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 //import logo from './logo.svg';
+//React is given class component
 import { Component } from 'react'
 import CardList from './components/card-list/card-list.component'
 import SearchBox from './components/search-box/search-box.component';
@@ -7,7 +8,9 @@ import './App.css';
 //and it also tie together reusable portion of code into one set
 //app component -- all of ui lies in this component
 class App extends Component{
+  //whenever this app is created instantiate first
   constructor(){
+    //super call other constructor methods that is instantiating
     super();
     //creating empty array to initialise a value
     this.state={
@@ -23,22 +26,21 @@ class App extends Component{
       .then((response)=>response.json())
       //whatever return from the response that get stored in user as a parameter
       .then((users)=>
-      this.setState(
-        ()=>{
-          return {monsters:users};
-        },
-        
+        this.setState(
+          ()=>{
+            return {monsters:users};
+          },
       ))
   }
-onsearchChange=(event)=>{
-  const searchField=event.target.value.toLocaleLowerCase();
-  this.setState(()=>{
-    return { searchField }
-  },
-  )
-}
+  onsearchChange=(event)=>{
+    const searchField=event.target.value.toLocaleLowerCase();
+      this.setState(()=>{
+        return { searchField }
+      },
+    )
+  }
   render(){
-    console.log('render from app.js')
+    //console.log('render from app.js')
     const {monsters,searchField}=this.state;
     const {onsearchChange} = this;
     //.filter gives back new array
@@ -46,16 +48,16 @@ onsearchChange=(event)=>{
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
-  return (
-    <div className="App">
-      <h1 className='app-title'>Monsters Rolodex</h1>
-      <SearchBox onChangeHandler={onsearchChange} 
-      placeholder='search-monsters'
-      className='search-box'/>
-      <CardList monsters={filteredMonsters}/>
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox onChangeHandler={onsearchChange} 
+        placeholder='search-monsters'
+        className='search-box'/>
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
 }
 
 export default App;
