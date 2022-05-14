a36fac7f771474b2296af98e34aadeadbed3e1c2
surbhi kumari <surbhikumaridav@gmail.com>
2022-05-14 11:59:52 +0530

    monsters first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { Component } from 'react'
+import CardList from './components/card-list/card-list.component'
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
+//component is meant to tie to tother functionala nd visual representation
+//and it also tie together reusable portion of code into one set
+//app component -- all of ui lies in this component
+class App extends Component{
+  constructor(){
+    super();
+    //creating empty array to initialise a value
+    this.state={
+      monsters:[],
+      searchField:''
+      };
+      
+  }
 
-function App() {
+  componentDidMount()
+  {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response)=>response.json())
+      //whatever return from the response that get stored in user as a parameter
+      .then((users)=>
+      this.setState(
+        ()=>{
+          return {monsters:users};
+        },
+        
+      ))
+  }
+onsearchChange=(event)=>{
+  const searchField=event.target.value.toLocaleLowerCase();
+  this.setState(()=>{
+    return { searchField }
+  },
+  )
+}
+  render(){
+    console.log('render from app.js')
+    const {monsters,searchField}=this.state;
+    const {onsearchChange} = this;
+    //.filter gives back new array
+    const filteredMonsters=monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox onChangeHandler={onsearchChange} 
+      placeholder='search-monsters'
+      className='search-box'/>
+      <CardList monsters={filteredMonsters}/>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import Card from '../card/card.component'
+import './card-list.styles.css'
+class CardList extends Component{
+    render(){
+        const {monsters}=this.props
+        //console.log('render from cardlist'+ this.props.monsters)
+        return (
+            <div className="card-list">
+                {monsters.map((monster)=>{
+                    return <Card monster={monster}/>
+                })}
+            </div>
+        );
+    }
+}
+
+export default CardList;
